28730fefb2e9a8de24e5e40e3b538966c6dd6bfa
kirananto <kirananto@gmail.com>
2015-07-04 12:23:34 +0530

    Change Compiler

--- build.sh
+++ build.sh
@@ -24,13 +24,13 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 # Modify the following variable if you want to build
-export CROSS_COMPILE="/home/kiran/android/toolchains/aarch64-linux-android-4.9-LTS/bin/aarch64-linux-android-"
+export CROSS_COMPILE="//home/kiran/android/toolchains/aarch64-linux-android-6.0/bin/aarch64-linux-android-"
 export USE_CCACHE=1
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER="Kiran.Anto"
 export KBUILD_BUILD_HOST="RaZor-Machine"
-STRIP="/home/kiran/android/toolchains/aarch64-linux-android-4.9-LTS/bin/aarch64-linux-android-strip"
+STRIP="/home/kiran/android/toolchains/aarch64-linux-android-6.0/bin/aarch64-linux-android-strip"
 MODULES_DIR=$KERNEL_DIR/../RaZORBUILDOUTPUT/Tomato
 
 compile_kernel ()
@@ -74,7 +74,7 @@ compile_kernel
 esac
 cp $KERNEL_DIR/arch/arm64/boot/Image  $MODULES_DIR/../tools
 mv $MODULES_DIR/../tools/Image $MODULES_DIR/../tools/zImage
-cp $MODULES_DIR $MODULES_DIR/../system/lib/modules
+cp $MODULES_DIR/wlan.ko $MODULES_DIR/../system/lib/modules/
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
