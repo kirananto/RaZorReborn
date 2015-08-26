1933bb6ca49fe46eaaf1e15119ca381e2ea2a59a
Kiran Anto <kirananto@gmail.com>
2015-08-26 17:49:50 +0530

    Add Build Script

---
+++ build.sh
@@ -0,0 +1,92 @@
+ #
+ # Copyright © 2014, KiranAnto
+ #
+ # Custom build script
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # Please maintain this if you use this script or any part of it
+ #
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/arch/arm64/boot/Image
+DTBTOOL=$KERNEL_DIR/tools/dtbToolCM
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+# Modify the following variable if you want to build
+export CROSS_COMPILE="../../Toolchains/aarch64-linux-android-6.0/bin/aarch64-linux-android-"
+export USE_CCACHE=1
+export ARCH=arm64
+export SUBARCH=arm
+export KBUILD_BUILD_USER="Kiran.Anto"
+export KBUILD_BUILD_HOST="RaZor-Machine"
+STRIP="../../../Toolchains/aarch64-linux-android-6.0/bin/aarch64-linux-android-strip"
+MODULES_DIR=$KERNEL_DIR/../RaZORBUILDOUTPUT/Common
+
+compile_kernel ()
+{
+rm $MODULES_DIR/../PLUTONIUM/tools/kernel
+rm $MODULES_DIR/../PLUTONIUM/tools/dt.img
+rm $KERNEL_DIR/arch/arm64/boot/Image
+rm $KERNEL_DIR/arch/arm64/boot/Image.gz
+rm $KERNEL_DIR/arch/arm64/boot/dts/*.dtb
+rm -rf $MODULES_DIR/../PLUTONIUM/system/lib/modules/*
+echo -e "**********************************************************************************************"
+echo "                    "
+echo "                                        Compiling RaZorReborn kernel                    "
+echo "                    "
+echo -e "**********************************************************************************************"
+make razor_plutonium_defconfig
+make -j12
+if ! [ -a $KERN_IMG ];
+then
+echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+exit 1
+fi
+$DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm64/boot/dts/
+strip_modules
+strip_modules
+}
+
+
+strip_modules ()
+{
+echo "Copying modules"
+rm $MODULES_DIR/*
+find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+cd $MODULES_DIR
+echo "Stripping modules for size"
+$STRIP --strip-unneeded *.ko
+cd $KERNEL_DIR
+}
+
+case $1 in
+clean)
+make ARCH=arm64 -j8 clean mrproper
+;;
+*)
+compile_kernel
+;;
+esac
+cp $KERNEL_DIR/arch/arm64/boot/Image  $MODULES_DIR/../PLUTONIUM/tools/kernel
+cp $KERNEL_DIR/arch/arm64/boot/dt.img $MODULES_DIR/../PLUTONIUM/tools/dt.img
+cp $MODULES_DIR/* $MODULES_DIR/../PLUTONIUM/system/lib/modules/
+cd $MODULES_DIR/../PLUTONIUM
+zipfile="RRV1OPO2UBER-$(date +"%Y-%m-%d(%I.%M%p)").zip"
+zip -r $zipfile system tools META-INF -x *kernel/.gitignore*
+dropbox_uploader -p upload $MODULES_DIR/../PLUTONIUM/$zipfile /
+dropbox_uploader share /$zipfile
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+echo "Enjoy RazorKernel"
