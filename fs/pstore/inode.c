1f2a035df95eeaf4b96dfab471b180cdfb83fadf
Kiran Anto <kirananto@gmail.com>
2015-08-27 06:37:43 +0530

    pstore: d_alloc_name() doesn't return an ERR_PTR
    commit c39524e6744284452ef45480d3153bec28960c32 upstream.
    
    d_alloc_name() returns NULL on error.  Also I changed the error code
    from -ENOSPC to -ENOMEM to reflect that we were short on RAM not disk
    space.

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -342,7 +342,7 @@ int pstore_mkfile(enum pstore_type_id type, char *psname, u64 id, int count,
 
 	rc = -ENOSPC;
 	dentry = d_alloc_name(root, name);
-	if (IS_ERR(dentry))
+	if (!dentry)
 		goto fail_lockedalloc;
 #ifdef VENDOR_EDIT  // modify by yangrujin@bsp for ramoops memcpy addr alignment 2015-05-27
         memcpy_pstore(private->data, data, size);
