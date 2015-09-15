cf29b5e0cb2014d149ff71e145aa59ff7bc8fddc
Alucard24 <dmbaoh2@gmail.com>
2015-09-15 22:15:40 +0530

    Introduce Darkness Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -434,6 +434,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #endif
 
 /*********************************************************************
