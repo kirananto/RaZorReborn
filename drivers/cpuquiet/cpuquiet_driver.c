d25c271b2bf785a8c51804c9ff9289d782050818
Kiran Anto <kirananto@gmail.com>
2015-09-15 22:33:25 +0530

    cpuquiet: fix inline functions

--- drivers/cpuquiet/cpuquiet_driver.c
+++ drivers/cpuquiet/cpuquiet_driver.c
@@ -36,8 +36,8 @@ static bool manual_hotplug = false;
 // core 0 is always active
 unsigned int cpu_core_state[3] = {0, 0, 0};
 		
-static unsigned int min_cpus = 1;
-static unsigned int max_cpus = CONFIG_NR_CPUS;
+unsigned int min_cpus = 1;
+unsigned int max_cpus = CONFIG_NR_CPUS;
 
 static bool log_hotplugging = false;
 #define hotplug_info(msg...) do { \
@@ -49,7 +49,7 @@ unsigned int cpq_available_cpus(void)
     return 4;
 }
 
-static inline unsigned int num_cpu_check(unsigned int num)
+inline unsigned int num_cpu_check(unsigned int num)
 {
 	if (num > cpq_available_cpus())
 		return cpq_available_cpus();
