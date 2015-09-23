ade9674fd0f1717970172115f842e5fabb4ed73d
Jamison904 <tinfamousprollc@gmail.com>
2015-09-23 06:06:53 +0530

    Add BioShock (CPU Governor)
    Signed off : Kiran Anto <kirananto@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -437,6 +437,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
 #endif
 
 /*********************************************************************
